3fe3b3d05d20b749db9ff7148b52f487d309eeef
Olha Kozmenkova <akozmenkova@ukr.net>
2026-03-10 20:46:25 +0100

    Added Tool component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import Greeting from "./components/Greeting/Greeting";
 import Hero from "./components/Hero/Hero";
+import Tool from "./components/Tool/Tool";
 
 function App() {
   return (
     <>
-      <Greeting />
+      <h1>Hello, React!</h1>
+      <Tool/>
+      <Greeting name="John" />
+      <Greeting name="Bob" />
+      <Greeting name="Ioann" age={20} />
       <Hero />
     </>
   );
 }
 
 export default App;
+
+// Hero - внутри section
+// Buy our unique product!
+
+// SPA single page application
